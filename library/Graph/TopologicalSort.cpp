183724a25eaa39ca4644a77db25da9e0c5b55bdd
AnandDev006 <anand.dev.006@gmail.com>
2020-04-12 23:21:53 +0530

    Topological Sort

---
+++ library/Graph/TopologicalSort.cpp
@@ -0,0 +1,97 @@
+/*
+    author : Anand
+    Topological sort
+*/
+
+#include <bits/stdc++.h>
+
+#define sz(a) int((a).size())
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp(a, b) make_pair(ll(a), ll(b))
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+#define present(c, x) ((c).find(x) != (c).end())
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+const int mod = 1000000007;
+const double zero = 10e-9;
+const int N = 3e5, M = N;
+
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs_topSort(int u, int par);
+
+vector<int> g[N];
+int DP[N];
+stack<int> topSort;
+int parent[N];
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("main.inp", "r", stdin);
+    freopen("main.out", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    ipgraph(n,m);
+    for(int i = 1;  i <= n ; ++i) {
+        if(parent[i]) continue;
+        parent[i] = -1;
+        dfs_topSort(i, -1);
+    }
+    while(!topSort.empty()) {
+        cout << topSort.top() << '\n';
+        topSort.pop();
+    }
+    return 0;
+}
+
+int mpow(int base, int exp) {
+    base %= mod;
+    int result = 1;
+    while (exp > 0) {
+        if (exp & 1) result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+void ipgraph(int n, int m) {
+    int i, u, v;
+    while (m--) {
+        cin >> u >> v;
+        g[u].pb(v);
+    }
+}
+
+void dfs_topSort(int u, int par) {
+    for (int v : g[u]) {
+        if (parent[v]) continue;
+        parent[v] = u;
+        dfs_topSort(v, u);
+    }
+    topSort.push(u);
+}
