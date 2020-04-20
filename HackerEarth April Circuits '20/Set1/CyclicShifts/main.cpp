311be1fd0b9422149193b108017132364501955e
anandd6 <anand.dev@ck12.org>
2020-04-21 00:13:44 +0530

    LA + HEL

---
+++ HackerEarth April Circuits '20/Set1/CyclicShifts/main.cpp
@@ -0,0 +1,83 @@
+/*
+    author : Anand
+
+*/
+
+#include <bits/stdc++.h>
+
+// #include <math.h>
+
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// #include <map>
+// #include <queue>
+// #include <set>
+// #include <stack>
+// #include <utility>
+// #include <vector>
+
+#define sz(a) int((a).size())
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp(a, b) make_pair(ll(a), ll(b))
+#define F first
+#define S second
+#define all(c) (c).begin(), (c).end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+#define present(c, x) ((c).find(x) != (c).end())
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+const int INF = 1e9 + 5;
+const int mod = 1000000007;
+const double zero = 10e-9;
+const int N = 3e5, M = N;
+
+int DP[N];
+
+void solve() {
+    int n, m;
+    char c;
+    cin >> n >> m >> c;
+    int X = pow(2, 16) - 1;
+    int ans;
+    if (c == 'L') {
+        ans = ((n << m) & X | (n >> (16 - m)));
+    } else {
+        ans = ((n >> m) | ((n << (16 - m)) & X));
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("main.inp", "r", stdin);
+    freopen("main.out", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 /*
     author : Anand
+    Return the root node of a binary search tree that matches the given preorder traversal.
+
+    (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+    
+
+    Example 1:
+
+    Input: [8,5,1,7,10,12]
+    Output: [8,5,10,1,7,null,12] -> BFS output
 
 */
 
@@ -19,72 +29,56 @@
 
 #define sz(a) int((a).size())
 #define ll long long
-#define si(x) scanf("%d", &x)
-#define sl(x) scanf("%lld", &x)
-#define ss(s) scanf("%s", s)
-#define pi(x) printf("%d\n", x)
-#define pl(x) printf("%lld\n", x)
-#define ps(s) printf("%s\n", s)
 #define pb push_back
 #define mp(a, b) make_pair(ll(a), ll(b))
 #define F first
 #define S second
 #define all(c) (c).begin(), (c).end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
 #define PI 3.1415926535897932384626
-#define present(c, x) ((c).find(x) != (c).end())
-#define cpresent(c, x) (find(all(c), x) != (c).end())
 
 using namespace std;
 
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-
 const int INF = 1e9 + 5;
-const int mod = 1000000007;
-const double zero = 10e-9;
+const int MOD = 1000000007;
+const double ZERO = 10e-9;
 const int N = 3e5, M = N;
 
-vector<int> g[N];
-int DP[N];
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-int mpow(int base, int exp) {
-    base %= mod;
-    int result = 1;
-    while (exp > 0) {
-        if (exp & 1) result = ((ll)result * base) % mod;
-        base = ((ll)base * base) % mod;
-        exp >>= 1;
+class Solution {
+    int pos;
+    int n;
+    int INF = 1e9 + 5;
+    TreeNode* makeBST(vector<int>& preorder, int low, int high) {
+        if (this->pos == this->n || !(preorder[this->pos] > low && preorder[this->pos] < high))
+            return NULL;
+        int value = preorder[this->pos];
+        TreeNode* node = new TreeNode(value);
+        this->pos++;
+        node->left = makeBST(preorder, low, value);
+        node->right = makeBST(preorder, value, high);
+        return node;
     }
-    return result;
-}
 
-void ipgraph(int n, int m) {
-    int i, u, v;
-    while (m--) {
-        cin >> u >> v;
-        g[u].pb(v);
-        g[v].pb(u);
+   public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int low = -INF, high = INF;
+        this->pos = 0;
+        this->n = preorder.size();
+        return makeBST(preorder, low, high);
     }
-}
-
-void dfs(int u, int par) {
-    for (int v : g[u]) {
-        if (v == par) continue;
-        dfs(v, u);
-    }
-}
+};
 
 int main() {
-    // #ifndef ONLINE_JUDGE
-    //     freopen("main.inp", "r", stdin);
-    //     freopen("main.out", "w", stdout);
-    // #endif
+#ifndef ONLINE_JUDGE
+    freopen("main.inp", "r", stdin);
+    freopen("main.out", "w", stdout);
+#endif
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
