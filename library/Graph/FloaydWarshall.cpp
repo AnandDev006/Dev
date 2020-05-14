cb00ebc4665fa6cdca87f82766fa65c28640e9f8
anandd6 <anand.dev@ck12.org>
2020-05-14 23:05:38 +0530

    update lb

--- library/Graph/FloaydWarshall.cpp
+++ library/Graph/FloaydWarshall.cpp
@@ -1,77 +1,28 @@
 /*
-    author : Anand
-    FloydWarshall
+  author : Anand
 */
 
 #include <bits/stdc++.h>
 
-#define sz(a) int((a).size())
-#define ll long long
-#define si(x) scanf("%d", &x)
-#define sl(x) scanf("%lld", &x)
-#define ss(s) scanf("%s", s)
-#define pi(x) printf("%d\n", x)
-#define pl(x) printf("%lld\n", x)
-#define ps(s) printf("%s\n", s)
-#define pb push_back
-#define mp(a, b) make_pair(ll(a), ll(b))
-#define F first
-#define S second
-#define all(c) (c).begin(), (c).end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define present(c, x) ((c).find(x) != (c).end())
-#define cpresent(c, x) (find(all(c), x) != (c).end())
-
 using namespace std;
 
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
+#define PI 3.1415926535897932384626
+#define int long long
+#define ll long long
 
-const int mod = 1000000007;
-const double zero = 10e-9;
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "\t" << #__VA_ARGS__ << " : " << (__VA_ARGS__) << endl;
+#else
+#define debug(...) 42
+#endif
+
+const int INF = 1e18 + 5;
+const int MOD = 1000000007;
 const int N = 3e3, M = N;
-
-int mpow(int base, int exp);
-void ipgraph(int n, int m);
-void floydWarshall(int n);
 
 int g[N][N];     // adjacency matrix
 int dist[N][N];  // distance matrix
 int DP[N];
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("main.inp", "r", stdin);
-    freopen("main.out", "w", stdout);
-#endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    ipgraph(n, m);
-    floydWarshall(n);
-    for (int i = 1; i <= n; ++i) {
-        cout << i << " : " << dist[1][i] << '\n';
-    }
-    return 0;
-}
-
-int mpow(int base, int exp) {
-    base %= mod;
-    int result = 1;
-    while (exp > 0) {
-        if (exp & 1) result = ((ll)result * base) % mod;
-        base = ((ll)base * base) % mod;
-        exp >>= 1;
-    }
-    return result;
-}
 
 void ipgraph(int n, int m) {
     int i, u, v, w;
@@ -103,4 +54,18 @@ void floydWarshall(int n) {
             }
         }
     }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    ipgraph(n, m);
+    floydWarshall(n);
+    for (int i = 1; i <= n; ++i) {
+        cout << i << " : " << dist[1][i] << '\n';
+    }
+    return 0;
 }
--- library/Graph/TopologicalSort.cpp
+++ library/Graph/TopologicalSort.cpp
@@ -1,89 +1,35 @@
 /*
-    author : Anand
-    Topological sort
+  author : Anand
 */
 
 #include <bits/stdc++.h>
 
-#define sz(a) int((a).size())
-#define ll long long
-#define si(x) scanf("%d", &x)
-#define sl(x) scanf("%lld", &x)
-#define ss(s) scanf("%s", s)
-#define pi(x) printf("%d\n", x)
-#define pl(x) printf("%lld\n", x)
-#define ps(s) printf("%s\n", s)
-#define pb push_back
-#define mp(a, b) make_pair(ll(a), ll(b))
-#define F first
-#define S second
-#define all(c) (c).begin(), (c).end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define present(c, x) ((c).find(x) != (c).end())
-#define cpresent(c, x) (find(all(c), x) != (c).end())
-
 using namespace std;
 
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
+#define PI 3.1415926535897932384626
+#define int long long
+#define ll long long
 
-const int mod = 1000000007;
-const double zero = 10e-9;
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "\t" << #__VA_ARGS__ << " : " << (__VA_ARGS__) << endl;
+#else
+#define debug(...) 42
+#endif
+
+const int INF = 1e18 + 5;
+const int MOD = 1000000007;
 const int N = 3e5, M = N;
-
-int mpow(int base, int exp);
-void ipgraph(int n, int m);
-void dfs_topSort(int u, int par);
 
 vector<int> g[N];
 int DP[N];
 stack<int> topSort;
 int parent[N];
 
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("main.inp", "r", stdin);
-    freopen("main.out", "w", stdout);
-#endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    ipgraph(n,m);
-    for(int i = 1;  i <= n ; ++i) {
-        if(parent[i]) continue;
-        parent[i] = -1;
-        dfs_topSort(i, -1);
-    }
-    while(!topSort.empty()) {
-        cout << topSort.top() << '\n';
-        topSort.pop();
-    }
-    return 0;
-}
-
-int mpow(int base, int exp) {
-    base %= mod;
-    int result = 1;
-    while (exp > 0) {
-        if (exp & 1) result = ((ll)result * base) % mod;
-        base = ((ll)base * base) % mod;
-        exp >>= 1;
-    }
-    return result;
-}
-
 void ipgraph(int n, int m) {
     int i, u, v;
     while (m--) {
         cin >> u >> v;
-        g[u].pb(v);
+        g[u].push_back(v);
     }
 }
 
@@ -94,4 +40,23 @@ void dfs_topSort(int u, int par) {
         dfs_topSort(v, u);
     }
     topSort.push(u);
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    ipgraph(n, m);
+    for (int i = 1; i <= n; ++i) {
+        if (parent[i]) continue;
+        parent[i] = -1;
+        dfs_topSort(i, -1);
+    }
+    while (!topSort.empty()) {
+        cout << topSort.top() << '\n';
+        topSort.pop();
+    }
+    return 0;
 }
