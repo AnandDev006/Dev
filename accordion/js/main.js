d8ee3a8a8b12b19b03a49727a0f54fb179e279b7
anandd6 <anand.dev@ck12.org>
2020-09-12 13:12:35 +0530

    Revert "update lb"
    This reverts commit cb00ebc4665fa6cdca87f82766fa65c28640e9f8.

--- accordion/js/main.js
+++
@@ -1,7 +0,0 @@
-const btns = document.getElementsByClassName("btn");
-
-Array.prototype.slice.call(btns).forEach((btn) =>
-  btn.addEventListener("click", function (e) {
-    this.nextElementSibling.classList.toggle("open");
-  })
-);
--- svg/1/js/main.js
+++
@@ -1,7 +0,0 @@
-const rect = document.getElementById("rect");
-rect.addEventListener("mouseenter", function () {
-  this.classList.add("magic");
-});
-rect.addEventListener("mouseleave", function () {
-  this.classList.remove("magic");
-});
