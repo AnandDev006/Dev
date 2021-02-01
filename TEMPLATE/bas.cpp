9b3b790bebb900e9c95f02c4450f96ee99c2c6e4
AnandDev006 <anand.dev.006@gmail.com>
2021-02-01 17:28:21 +0530

    new debug

--- TEMPLATE/bas.cpp
+++ TEMPLATE/bas.cpp
@@ -10,11 +10,32 @@ using namespace std;
 #define int long long
 #define ll long long
 
+/* <DEBUG> */
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+  os << '{';
+  string sep;
+  for (const T &x : v) os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
 #ifndef ONLINE_JUDGE
-#define debug(...) cerr << "\t" << #__VA_ARGS__ << " : " << (__VA_ARGS__) << endl;
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
 #define debug(...) 42
 #endif
+
+/* </DEBUG> */
 
 const int INF = 1e18 + 5;
 const int MOD = 1000000007;
@@ -22,7 +43,6 @@ const int N = 1e7;
 const int K = 25;
 
 void solve(int tc = 1) {
-  
 }
 
 signed main() {
