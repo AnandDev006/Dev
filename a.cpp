49d2339101466b552ec54775646689e21d6a07c9
AnandDev006 <anand.dev.006@gmail.com>
2021-03-07 15:46:37 +0530

    git ignore[D

--- a.cpp
+++ a.cpp
@@ -15,20 +15,24 @@ const int N = 1e7;
 const int K = 25;
 
 void solve(int tc = 1) {
-  int n;
-  cin >> n;
-
-  vector<int> x(n), y(n);
-  for (int i = 0; i < n; ++i)
-    cin >> x[i] >> y[i];
-
-  if (n & 1) {
-    cout << 1 << "\n";
-  } else {
-    sort(x.begin(), x.end());
-    sort(y.begin(), y.end());
-    cout << (x[n / 2] - x[n / 2 - 1] + 1) * (y[n / 2] - y[n / 2 - 1] + 1) << "\n";
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n, 0), b(n, 0), pref1(n+1, 0), pref2(n+1, 0);
+  for(int i = 0 ; i < n ; ++i) 
+    cin >> a[i];
+  for(int i = 0 ; i < n ; ++i)
+    cin >> b[i];
+  for(int i = 1 ; i <=n ; ++i) {
+    pref1[i] = pref1[i-1] + (b[i-1] * a[i-1]);
+    pref2[i] = pref2[i-1] + a[i-1];
   }
+  int ans = 0;
+  for(int i = k ; i <= n ; ++i) {
+    // int lec =   [0,i-k] [i-k,i] [i, n]
+    int lec = pref1[i-k] + (pref2[i] - pref2[i-k]) + (pref1[n] - pref1[i]);
+    ans = max(ans, lec);
+  }
+  cout << ans << '\n';
 }
 
 signed main() {
@@ -36,7 +40,7 @@ signed main() {
   ios::sync_with_stdio(false);
 
   int tc = 1;
-  cin >> tc;
+  // cin >> tc;
   for (int t = 1; t <= tc; ++t) solve(t);
 
   return 0;
